ced50ee241a8c1b7071fa3e61863057eda43d0b2
linawolf <48202465+linawolf@users.noreply.github.com>
2020-12-30 15:24:51 +0100

    [TASK] Fix deprecations for 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '10.4.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '11.0.0-11.99.99',
         ]
     ],
     'autoload' => [
