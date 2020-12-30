e3f4bb43086c2d05efd6ca2245ea9fbf1bbcc53b
linawolf <48202465+linawolf@users.noreply.github.com>
2020-12-30 16:35:02 +0100

    improve composer.json, set TYPO3 Documentation Team as author, use the  skeleton of the ext_localconf.php (https://docs.typo3.org/m/typo3/reference-coreapi/master/en-us/ExtensionArchitecture/ConfigurationFiles/Index.html)

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Store Inventory',
-    'description' => 'An extension to manage a stock.',
+    'description' => 'This TYPO3 extension is an example in the Extbase Fluid Book. See external documentation',
     'category' => 'plugin',
     'author' => 'TYPO3 Documentation Team',
     'author_company' => 'TYPO3',
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,18 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
+// Prevent script from being called directly
+defined('TYPO3') or die();
 
-call_user_func(
-    function () {
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'StoreInventory',
-            'Pi1',
-            [
-                \T3docs\StoreInventory\Controller\StoreInventoryController::class => 'list',
-            ],
-            // non-cacheable actions
-            [
-                \T3docs\StoreInventory\Controller\StoreInventoryController::class => '',
-            ]
-        );
-    }
-);
+// encapsulate all locally defined variables
+(function () {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+        'StoreInventory',
+        'Pi1',
+        [
+            \T3docs\StoreInventory\Controller\StoreInventoryController::class => 'list',
+        ],
+        // non-cacheable actions
+        [
+            \T3docs\StoreInventory\Controller\StoreInventoryController::class => '',
+        ]
+    );
+})();
