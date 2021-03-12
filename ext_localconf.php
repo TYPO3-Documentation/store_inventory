df064d6976dfa293ab6766308dfa9210c749ea5a
Franz Holzinger <franz.holzinger@festwein.de>
2021-03-12 21:51:16 +0100

    ananymous function

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,22 @@
 <?php
 // Prevent script from being called directly
+
 defined('TYPO3') or die();
 
 // encapsulate all locally defined variables
-(function () {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'StoreInventory',
-        'Pi1',
-        [
-            \T3docs\StoreInventory\Controller\StoreInventoryController::class => 'list',
-        ],
-        // non-cacheable actions
-        [
-            \T3docs\StoreInventory\Controller\StoreInventoryController::class => '',
-        ]
-    );
-})();
+call_user_func(
+   function ()
+   {
+   \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+      'StoreInventory',
+      'Pi1',
+      [
+         \T3docs\StoreInventory\Controller\StoreInventoryController::class => 'list',
+      ],
+      // non-cacheable actions
+      [
+         \T3docs\StoreInventory\Controller\StoreInventoryController::class => '',
+      ]
+   );
+  }
+);
